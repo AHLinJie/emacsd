a4dcc3eb18ab4de2fa137bf6939908623276e1c5
ahlinjie <1051631394@qq.com>
2016-01-04 18:01:49 +0800

    my emacs dir

---
+++ el-get/.loaddefs.el
@@ -0,0 +1,528 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (arx-builder arx-or arx-and define-arx) "ample-regexps/ample-regexps"
+;;;;;;  "ample-regexps/ample-regexps.el" (22154 15441 53934 802000))
+;;; Generated autoloads from ample-regexps/ample-regexps.el
+
+(autoload 'define-arx "ample-regexps/ample-regexps" "\
+Generate a custom rx-like macro under name MACRO.
+
+See `rx' for how the generated macro can be invoked.
+
+FORM-DEFS is a list of custom s-exp definitions to create whose
+elements have the form (SYM DEF), where DEF is one of
+the following:
+
+- \"LITERAL\" -- create a matcher to match a string literally
+
+- (regexp \"LITERAL\") -- create a match given a regexp
+
+- SYMBOL -- create an alias for a symbol either defined earlier
+  on the list or provided by `rx'
+
+- (SUBFORM ...) -- create an alias for an application of s-exp
+  subform either defined earlier on the list or provided by `rx'
+
+- (:func #'FORM-FUNC ...) -- create an s-exp definition
+
+The most interesting here is the last variant.  When a
+corresponding rx form will be encountered, FORM-FUNC will be
+called with all elements of that form as arguments (with the
+first one being the form symbol itself).  FORM-FUNC must then
+return a valid s-exp or a properly grouped plain regexp.
+
+Another keywords that are recognized in the plist are:
+- :min-args -- minimum number of arguments for that form (default nil)
+- :max-args -- maximum number of arguments for that form (default nil)
+- :predicate -- if given, all rx form arguments must satisfy it
+
+\(fn MACRO FORM-DEFS)" nil t)
+
+(autoload 'arx-and "ample-regexps/ample-regexps" "\
+Generate an expression to match a sequence of FORMS.
+
+\(fn FORMS)" nil nil)
+
+(autoload 'arx-or "ample-regexps/ample-regexps" "\
+Generate an expression to match one of FORMS.
+
+\(fn FORMS)" nil nil)
+
+(autoload 'arx-builder "ample-regexps/ample-regexps" "\
+Run `re-builder' using arx form named ARX-NAME.
+
+\(fn &optional ARX-NAME)" t nil)
+
+;;;***
+
+;;;### (autoloads (global-auto-complete-mode auto-complete-mode auto-complete)
+;;;;;;  "auto-complete/auto-complete" "auto-complete/auto-complete.el"
+;;;;;;  (22001 38682 981840 649000))
+;;; Generated autoloads from auto-complete/auto-complete.el
+
+(autoload 'auto-complete "auto-complete/auto-complete" "\
+Start auto-completion at current point.
+
+\(fn &optional SOURCES)" t nil)
+
+(autoload 'auto-complete-mode "auto-complete/auto-complete" "\
+AutoComplete mode
+
+\(fn &optional ARG)" t nil)
+
+(defvar global-auto-complete-mode nil "\
+Non-nil if Global-Auto-Complete mode is enabled.
+See the command `global-auto-complete-mode' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `global-auto-complete-mode'.")
+
+(custom-autoload 'global-auto-complete-mode "auto-complete/auto-complete" nil)
+
+(autoload 'global-auto-complete-mode "auto-complete/auto-complete" "\
+Toggle Auto-Complete mode in all buffers.
+With prefix ARG, enable Global-Auto-Complete mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Auto-Complete mode is enabled in all buffers where
+`auto-complete-mode-maybe' would do it.
+See `auto-complete-mode' for more information on Auto-Complete mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (ac-config-default) "auto-complete/auto-complete-config"
+;;;;;;  "auto-complete/auto-complete-config.el" (22001 38682 981840
+;;;;;;  649000))
+;;; Generated autoloads from auto-complete/auto-complete-config.el
+
+(autoload 'ac-config-default "auto-complete/auto-complete-config" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads (el-get el-get-self-checksum el-get-checksum el-get-make-recipes
+;;;;;;  el-get-cd el-get-reinstall el-get-remove el-get-self-update
+;;;;;;  el-get-update-packages-of-type el-get-update-all el-get-update
+;;;;;;  el-get-install el-get-version) "el-get/el-get" "el-get/el-get.el"
+;;;;;;  (21999 37363 959108 417000))
+;;; Generated autoloads from el-get/el-get.el
+
+(autoload 'el-get-version "el-get/el-get" "\
+Message the current el-get version
+
+\(fn)" t nil)
+
+(autoload 'el-get-install "el-get/el-get" "\
+Cause the named PACKAGE to be installed after all of its
+dependencies (if any).
+
+PACKAGE may be either a string or the corresponding symbol.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-update "el-get/el-get" "\
+Update PACKAGE.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-update-all "el-get/el-get" "\
+Performs update of all installed packages.
+
+\(fn &optional NO-PROMPT)" t nil)
+
+(autoload 'el-get-update-packages-of-type "el-get/el-get" "\
+Update all installed packages of type TYPE.
+
+\(fn TYPE)" t nil)
+
+(autoload 'el-get-self-update "el-get/el-get" "\
+Update el-get itself.  The standard recipe takes care of reloading the code.
+
+\(fn)" t nil)
+
+(autoload 'el-get-remove "el-get/el-get" "\
+Remove any PACKAGE that is know to be installed or required.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-reinstall "el-get/el-get" "\
+Remove PACKAGE and then install it again.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-cd "el-get/el-get" "\
+Open dired in the package directory.
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-make-recipes "el-get/el-get" "\
+Loop over `el-get-sources' and write a recipe file for each
+entry which is not a symbol and is not already a known recipe.
+
+\(fn &optional DIR)" t nil)
+
+(autoload 'el-get-checksum "el-get/el-get" "\
+Compute the checksum of the given package, and put it in the kill-ring
+
+\(fn PACKAGE)" t nil)
+
+(autoload 'el-get-self-checksum "el-get/el-get" "\
+Compute the checksum of the running version of el-get itself.
+
+Also put the checksum in the kill-ring.
+
+\(fn)" t nil)
+
+(autoload 'el-get "el-get/el-get" "\
+Ensure that packages have been downloaded once and init them as needed.
+
+This will not update the sources by using `apt-get install' or
+`git pull', but it will ensure that:
+
+* the packages have been installed
+* load-path is set so their elisp files can be found
+* Info-directory-list is set so their info files can be found
+* Autoloads have been prepared and evaluated for each package
+* Any post-installation setup (e.g. `(require 'feature)') happens
+
+When SYNC is nil (the default), all installations run
+concurrently, in the background.
+
+When SYNC is 'sync, each package will be installed synchronously,
+and any error will stop it all.
+
+Please note that the `el-get-init' part of `el-get' is always
+done synchronously. There's `byte-compile' support though, and
+the packages you use are welcome to use `autoload' too.
+
+PACKAGES is expected to be a list of packages you want to install
+or init.  When PACKAGES is omited (the default), the list of
+already installed packages is considered.
+
+\(fn &optional SYNC &rest PACKAGES)" nil nil)
+
+;;;***
+
+;;;### (autoloads (el-get-bundle! el-get-bundle el-get-bundle-el-get)
+;;;;;;  "el-get/el-get-bundle" "el-get/el-get-bundle.el" (21999 37363
+;;;;;;  959108 417000))
+;;; Generated autoloads from el-get/el-get-bundle.el
+
+(autoload 'el-get-bundle-el-get "el-get/el-get-bundle" "\
+
+
+\(fn SRC)" nil nil)
+
+(autoload 'el-get-bundle "el-get/el-get-bundle" "\
+Install PACKAGE and run initialization FORM.
+
+PACKAGE can be either a simple package name or a package name
+with a modifier before the name to specify local recipe source
+information:
+
+* `<owner>/' : specifies a Github owner name
+* `gist:<id>' : specifies a Gist ID
+* `<type>:' : specifies a type of the package source
+
+If `FEATURE in PACKAGE' form is used instead of PACKAGE, then
+that FEATURE is `require'd after installing PACKAGE.  You can
+also use `el-get-bundle!' macro if FEATURE and PACKAGE are the
+same.  If you wish to `require' more than one feature, then use
+`:features' property in FORM.
+
+The initialization FORM may start with a property list that
+describes a local recipe.  The FORM after the property list is
+treated as initialization code, which is actually an `:after'
+property of the local recipe.
+
+A copy of the initialization code is stored in a directory
+specified by `el-get-bundle-init-directory' and its byte-compiled
+version is used if `el-get-bundle-byte-compile' is non-nil.
+
+\(fn PACKAGE &rest FORM)" nil t)
+
+(put 'el-get-bundle 'lisp-indent-function 'defun)
+
+(autoload 'el-get-bundle! "el-get/el-get-bundle" "\
+Install PACKAGE and run initialization form.
+It is the same as `el-get-bundle' except that PACKAGE is explicitly
+required.
+
+\(fn PACKAGE &rest ARGS)" nil t)
+
+(put 'el-get-bundle! 'lisp-indent-function 'defun)
+
+;;;***
+
+;;;### (autoloads (el-get-check-recipe) "el-get/el-get-check" "el-get/el-get-check.el"
+;;;;;;  (21999 37363 959108 417000))
+;;; Generated autoloads from el-get/el-get-check.el
+
+(autoload 'el-get-check-recipe "el-get/el-get-check" "\
+Check the format of the recipe.
+Please run this command before sending a pull request.
+Usage: M-x el-get-check-recipe RET
+
+You can run this function from checker script like this:
+    test/check-recipe.el PATH/TO/RECIPE.rcp
+
+When used as a lisp function, FILE-OR-BUFFER must be a buffer
+object or a file path.
+
+\(fn FILE-OR-BUFFER)" t nil)
+
+;;;***
+
+;;;### (autoloads (el-get-list-packages) "el-get/el-get-list-packages"
+;;;;;;  "el-get/el-get-list-packages.el" (21999 37363 959108 417000))
+;;; Generated autoloads from el-get/el-get-list-packages.el
+
+(autoload 'el-get-list-packages "el-get/el-get-list-packages" "\
+Display a list of packages.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (lua-start-process lua-mode) "lua-mode/lua-mode"
+;;;;;;  "lua-mode/lua-mode.el" (22154 15496 665933 197000))
+;;; Generated autoloads from lua-mode/lua-mode.el
+
+(autoload 'lua-mode "lua-mode/lua-mode" "\
+Major mode for editing Lua code.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(defalias 'run-lua #'lua-start-process)
+
+(autoload 'lua-start-process "lua-mode/lua-mode" "\
+Start a lua process named NAME, running PROGRAM.
+PROGRAM defaults to NAME, which defaults to `lua-default-application'.
+When called interactively, switch to the process buffer.
+
+\(fn &optional NAME PROGRAM STARTFILE &rest SWITCHES)" t nil)
+
+;;;***
+
+;;;### (autoloads (gfm-mode markdown-mode) "markdown-mode/markdown-mode"
+;;;;;;  "markdown-mode/markdown-mode.el" (22073 28357 628795 90000))
+;;; Generated autoloads from markdown-mode/markdown-mode.el
+
+(autoload 'markdown-mode "markdown-mode/markdown-mode" "\
+Major mode for editing Markdown files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode/markdown-mode" "\
+Major mode for editing GitHub Flavored Markdown files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (pymacs-apply pymacs-call pymacs-exec pymacs-eval
+;;;;;;  pymacs-autoload pymacs-load) "pymacs/pymacs" "pymacs/pymacs.el"
+;;;;;;  (22001 39477 169817 0))
+;;; Generated autoloads from pymacs/pymacs.el
+
+(autoload 'pymacs-load "pymacs/pymacs" "\
+Import the Python module named MODULE into Emacs.
+Each function in the Python module is made available as an Emacs function.
+The Lisp name of each function is the concatenation of PREFIX with
+the Python name, in which underlines are replaced by dashes.  If PREFIX is
+not given, it defaults to MODULE followed by a dash.
+If NOERROR is not nil, do not raise error when the module is not found.
+
+\(fn MODULE &optional PREFIX NOERROR)" t nil)
+
+(autoload 'pymacs-autoload "pymacs/pymacs" "\
+Pymacs's equivalent of the standard emacs facility `autoload'.
+Define FUNCTION to autoload from Python MODULE using PREFIX.
+If PREFIX is not given, it defaults to MODULE followed by a dash.
+Optional DOCSTRING documents FUNCTION until it gets loaded.
+INTERACTIVE is normally the argument to the function `interactive',
+t means `interactive' without arguments, nil means not interactive,
+which is the default.
+
+\(fn FUNCTION MODULE &optional PREFIX DOCSTRING INTERACTIVE)" nil nil)
+
+(autoload 'pymacs-eval "pymacs/pymacs" "\
+Compile TEXT as a Python expression, and return its value.
+
+\(fn TEXT)" t nil)
+
+(autoload 'pymacs-exec "pymacs/pymacs" "\
+Compile and execute TEXT as a sequence of Python statements.
+This functionality is experimental, and does not appear to be useful.
+
+\(fn TEXT)" t nil)
+
+(autoload 'pymacs-call "pymacs/pymacs" "\
+Return the result of calling a Python function FUNCTION over ARGUMENTS.
+FUNCTION is a string denoting the Python function, ARGUMENTS are separate
+Lisp expressions, one per argument.  Immutable Lisp constants are converted
+to Python equivalents, other structures are converted into Lisp handles.
+
+\(fn FUNCTION &rest ARGUMENTS)" nil nil)
+
+(autoload 'pymacs-apply "pymacs/pymacs" "\
+Return the result of calling a Python function FUNCTION over ARGUMENTS.
+FUNCTION is a string denoting the Python function, ARGUMENTS is a list of
+Lisp expressions.  Immutable Lisp constants are converted to Python
+equivalents, other structures are converted into Lisp handles.
+
+\(fn FUNCTION ARGUMENTS)" nil nil)
+
+;;;***
+
+;;;### (autoloads (venv-initialize-interactive-shells venv-shell-init
+;;;;;;  venv-cpvirtualenv venv-cdvirtualenv venv-lsvirtualenv venv-rmvirtualenv
+;;;;;;  venv-mkvirtualenv venv-workon venv-deactivate) "virtualenvwrapper/virtualenvwrapper"
+;;;;;;  "virtualenvwrapper/virtualenvwrapper.el" (22002 53648 850636
+;;;;;;  760000))
+;;; Generated autoloads from virtualenvwrapper/virtualenvwrapper.el
+
+(autoload 'venv-deactivate "virtualenvwrapper/virtualenvwrapper" "\
+Deactivate the current venv.
+
+\(fn)" t nil)
+
+(autoload 'venv-workon "virtualenvwrapper/virtualenvwrapper" "\
+Interactively switch to virtualenv NAME. Prompts for name if called
+interactively.
+
+\(fn &optional NAME)" t nil)
+
+(autoload 'venv-mkvirtualenv "virtualenvwrapper/virtualenvwrapper" "\
+Create new virtualenvs NAMES. If venv-location is a single
+directory, the new virtualenvs are made there; if it is a list of
+directories, the new virtualenvs are made in the current
+default-directory.
+
+\(fn &rest NAMES)" t nil)
+
+(autoload 'venv-rmvirtualenv "virtualenvwrapper/virtualenvwrapper" "\
+Delete virtualenvs NAMES.
+
+\(fn &rest NAMES)" t nil)
+
+(autoload 'venv-lsvirtualenv "virtualenvwrapper/virtualenvwrapper" "\
+List all available virtualenvs in a temp buffer.
+
+\(fn)" t nil)
+
+(autoload 'venv-cdvirtualenv "virtualenvwrapper/virtualenvwrapper" "\
+Change to the directory of current virtualenv. If
+SUBDIR is passed, append that to the path such that
+we are immediately in that directory.
+
+\(fn &optional SUBDIR)" t nil)
+
+(autoload 'venv-cpvirtualenv "virtualenvwrapper/virtualenvwrapper" "\
+Copy virtualenv NAME to NEWNAME. Any arguments not passed will be
+prompted for This comes with the same caveat as cpvirtualenv in the
+original virtualenvwrapper, which is that is far from guarenteed to
+work well. Many packages hardcode absolute paths in various places an
+will break if moved to a new location. Use with caution. If used with
+a single virtualenv directory, behaves just like cpvirtualenv in
+virtualenvwrapper.sh.  If used with virtualenvs spread around the
+filesystem, creates the new virtualenv in the current default
+directory.
+
+\(fn &optional NAME NEWNAME)" t nil)
+
+(autoload 'venv-shell-init "virtualenvwrapper/virtualenvwrapper" "\
+Activate the current virtualenv in a newly opened shell.
+
+\(fn PROCESS)" nil nil)
+
+(autoload 'venv-initialize-interactive-shells "virtualenvwrapper/virtualenvwrapper" "\
+Configure interactive shells for use with
+virtualenvwrapper.el.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads (yas-global-mode yas-minor-mode) "yasnippet/yasnippet"
+;;;;;;  "yasnippet/yasnippet.el" (22001 39813 785808 26000))
+;;; Generated autoloads from yasnippet/yasnippet.el
+
+(autoload 'yas-minor-mode "yasnippet/yasnippet" "\
+Toggle YASnippet mode.
+
+When YASnippet mode is enabled, `yas-expand', normally bound to
+the TAB key, expands snippets of code depending on the major
+mode.
+
+With no argument, this command toggles the mode.
+positive prefix argument turns on the mode.
+Negative prefix argument turns off the mode.
+
+Key bindings:
+\\{yas-minor-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+(defvar yas-global-mode nil "\
+Non-nil if Yas-Global mode is enabled.
+See the command `yas-global-mode' for a description of this minor mode.
+Setting this variable directly does not take effect;
+either customize it (see the info node `Easy Customization')
+or call the function `yas-global-mode'.")
+
+(custom-autoload 'yas-global-mode "yasnippet/yasnippet" nil)
+
+(autoload 'yas-global-mode "yasnippet/yasnippet" "\
+Toggle Yas minor mode in all buffers.
+With prefix ARG, enable Yas-Global mode if ARG is positive;
+otherwise, disable it.  If called from Lisp, enable the mode if
+ARG is omitted or nil.
+
+Yas minor mode is enabled in all buffers where
+`yas-minor-mode-on' would do it.
+See `yas-minor-mode' for more information on Yas minor mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ample-regexps/init-tryout.el" "ample-regexps/test-arx.el"
+;;;;;;  "auto-complete/auto-complete-pkg.el" "dash/dash-functional.el"
+;;;;;;  "dash/dash.el" "el-get/el-get-autoloading.el" "el-get/el-get-build.el"
+;;;;;;  "el-get/el-get-byte-compile.el" "el-get/el-get-core.el" "el-get/el-get-custom.el"
+;;;;;;  "el-get/el-get-dependencies.el" "el-get/el-get-install.el"
+;;;;;;  "el-get/el-get-methods.el" "el-get/el-get-notify.el" "el-get/el-get-recipes.el"
+;;;;;;  "el-get/el-get-status.el" "fuzzy/fuzzy.el" "lua-mode/init-tryout.el"
+;;;;;;  "markdown-preview-mode/markdown-preview-mode.el" "popup/popup.el"
+;;;;;;  "s/s.el" "w3m-session/w3m-session.el" "websocket/websocket-functional-test.el"
+;;;;;;  "websocket/websocket-test.el" "websocket/websocket.el" "yasnippet/yasnippet-debug.el"
+;;;;;;  "yasnippet/yasnippet-tests.el") (22154 15497 157738 481000))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
---
+++ el-get/.status.el
@@ -0,0 +1,98 @@
+((ample-regexps status "installed" recipe
+		(:name ample-regexps :description "Compose and reuse Emacs regular expressions with ease" :type github :pkgname "immerrr/ample-regexps.el"))
+ (auto-complete status "installed" recipe
+		(:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+		       (popup fuzzy)
+		       :features auto-complete-config :post-init
+		       (progn
+			 (add-to-list 'ac-dictionary-directories
+				      (expand-file-name "dict" default-directory))
+			 (ac-config-default))))
+ (cl-lib status "installed" recipe
+	 (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (dash status "installed" recipe
+       (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+		("el-get.*\\.el$" "methods/")
+		:features el-get :post-init
+		(when
+		    (memq 'el-get
+			  (bound-and-true-p package-activated-list))
+		  (message "Deleting melpa bootstrap el-get")
+		  (unless package--initialized
+		    (package-initialize t))
+		  (when
+		      (package-installed-p 'el-get)
+		    (let
+			((feats
+			  (delete-dups
+			   (el-get-package-features
+			    (el-get-elpa-package-directory 'el-get)))))
+		      (el-get-elpa-delete-package 'el-get)
+		      (dolist
+			  (feat feats)
+			(unload-feature feat t))))
+		  (require 'el-get))))
+ (emacs-w3m status "required" recipe nil)
+ (fuzzy status "installed" recipe
+	(:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (lua-mode status "installed" recipe
+	   (:name lua-mode :description "A major-mode for editing Lua scripts" :depends
+		  (ample-regexps)
+		  :type github :pkgname "immerrr/lua-mode"))
+ (markdown-mode status "installed" recipe
+		(:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :website "http://jblevins.org/projects/markdown-mode/" :type git :url "git://jblevins.org/git/markdown-mode.git" :prepare
+		       (add-to-list 'auto-mode-alist
+				    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
+ (markdown-preview-mode status "installed" recipe
+			(:name markdown-preview-mode :description "Markdown preview mode with websocket.el" :type github :depends
+			       (websocket markdown-mode)
+			       :website "https://github.com/ancane/markdown-preview-mode.git" :pkgname "ancane/markdown-preview-mode"))
+ (popup status "installed" recipe
+	(:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :depends cl-lib :pkgname "auto-complete/popup-el"))
+ (pymacs status "installed" recipe
+	 (:name pymacs :description "Interface between Emacs Lisp and Python" :type github :pkgname "pinard/Pymacs" :prepare
+		(progn
+		  (el-get-envpath-prepend "PYTHONPATH" default-directory)
+		  (autoload 'pymacs-load "pymacs" nil t)
+		  (autoload 'pymacs-eval "pymacs" nil t)
+		  (autoload 'pymacs-exec "pymacs" nil t)
+		  (autoload 'pymacs-call "pymacs")
+		  (autoload 'pymacs-apply "pymacs"))
+		:build
+		(("make"))))
+ (rope status "installed" recipe
+       (:name rope :description "A python refactoring library" :post-init
+	      (el-get-envpath-prepend "PYTHONPATH" default-directory)
+	      :type git :url "https://github.com/python-rope/rope.git"))
+ (ropemacs status "installed" recipe
+	   (:name ropemacs :description "An Emacs minor mode for using rope python refactoring library in emacs." :post-init
+		  (progn
+		    (unless
+			(boundp 'pymacs-load-path)
+		      (setq pymacs-load-path nil))
+		    (add-to-list 'pymacs-load-path default-directory))
+		  :depends
+		  (rope ropemode pymacs)
+		  :type git :url "https://github.com/python-rope/ropemacs"))
+ (ropemode status "installed" recipe
+	   (:name ropemode :description "Common parts of ropemacs and ropevim." :post-init
+		  (progn
+		    (unless
+			(boundp 'pymacs-load-path)
+		      (setq pymacs-load-path nil))
+		    (add-to-list 'pymacs-load-path default-directory))
+		  :type git :url "https://github.com/python-rope/ropemode"))
+ (s status "installed" recipe
+    (:name s :description "The long lost Emacs string manipulation library." :type github :pkgname "magnars/s.el"))
+ (virtualenvwrapper status "installed" recipe
+		    (:name virtualenvwrapper :type github :website "https://github.com/porterjamesj/virtualenvwrapper.el" :description "virtualenv tool for emacs" :pkgname "porterjamesj/virtualenvwrapper.el" :depends
+			   (dash s)))
+ (w3m-session status "installed" recipe
+	      (:name w3m-session :description "Persistent emacs-w3m sessions" :type emacswiki :features w3m-session))
+ (websocket status "installed" recipe
+	    (:name websocket :description "A websocket implementation in elisp, for emacs." :type github :pkgname "ahyatt/emacs-websocket"))
+ (yasnippet status "installed" recipe
+	    (:name yasnippet :website "https://github.com/capitaomorte/yasnippet.git" :description "YASnippet is a template system for Emacs." :type github :pkgname "capitaomorte/yasnippet" :compile "yasnippet.el" :submodule nil :build
+		   (("git" "submodule" "update" "--init" "--" "snippets")))))
